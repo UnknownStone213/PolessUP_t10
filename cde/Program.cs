3658dd79b9f8abfa2d375da5f84867d412d2d565
Vlad <vlad.khvesyukovich@gmail.com>
2023-04-06 12:58:28 +0300

    t10 done

---
+++ cde/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cde
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter r:");
+            int r = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Calculate(n, a, r));
+
+            Console.ReadLine();
+
+            int Calculate(int amount, int first, int second)
+            {
+                int sum = 0;
+                for (int i = 0; i < amount; i++)
+                {
+                    sum += first + second * i;
+                }
+                return sum;
+            }
+        }
+    }
+}
